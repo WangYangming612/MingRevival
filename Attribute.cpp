6cddc27cf711a07a62a29e0c6375cc74d88e423e
晟煜 赵 <19282269230@163.com>
2026-04-04 17:44:38 +0800

    v2.0 核心面向对象属性类封装完成添加项目文件。

---
+++ Attribute.cpp
@@ -0,0 +1,67 @@
+﻿#include "Attribute.h"
+
+// 基类构造函数
+Attribute::Attribute(double initValue, double min, double max)
+    : m_value(initValue), m_min(min), m_max(max)
+{
+}
+
+// 基类虚析构函数
+Attribute::~Attribute()
+{
+}
+
+// 基类通用方法：增加数值，自动限制边界
+void Attribute::addValue(double num)
+{
+    m_value += num;
+    if (m_value > m_max) m_value = m_max;
+    if (m_value < m_min) m_value = m_min;
+}
+
+// 基类通用方法：减少数值，自动限制边界
+void Attribute::reduceValue(double num)
+{
+    m_value -= num;
+    if (m_value < m_min) m_value = m_min;
+    if (m_value > m_max) m_value = m_max;
+}
+
+// 基类get/set方法，封装属性
+double Attribute::getValue() const
+{
+    return m_value;
+}
+
+void Attribute::setValue(double value)
+{
+    m_value = value;
+}
+
+double Attribute::getMin() const
+{
+    return m_min;
+}
+
+double Attribute::getMax() const
+{
+    return m_max;
+}
+
+// 派生类1：皇帝属性重写，检查是否低于0
+bool EmperorAttr::checkLimit()
+{
+    return m_value > m_min;
+}
+
+// 派生类2：国家属性重写，检查是否低于0
+bool CountryAttr::checkLimit()
+{
+    return m_value > m_min;
+}
+
+// 派生类3：势力属性重写，检查是否超过上限
+bool ForceAttr::checkLimit()
+{
+    return m_value < m_max;
+}
---
+++ Attribute.h
@@ -0,0 +1,51 @@
+﻿#ifndef ATTRIBUTE_H
+#define ATTRIBUTE_H
+
+#include <QString>
+
+class Attribute
+{
+public:
+    Attribute(double initValue, double min, double max);
+    virtual ~Attribute();
+
+    virtual void addValue(double num);  // 增加数值
+    virtual void reduceValue(double num); // 减少数值
+    virtual bool checkLimit() = 0; // 检查数值是否越界
+
+    double getValue() const;
+    void setValue(double value);
+    double getMin() const;
+    double getMax() const;
+
+protected:
+    double m_value; // 当前数值
+    double m_min;   // 最小值边界
+    double m_max;   // 最大值边界
+};
+
+// 派生类1：崇祯皇帝个人属性
+class EmperorAttr : public Attribute
+{
+public:
+    EmperorAttr(double initValue = 50, double min = 0, double max = 100);
+    bool checkLimit() override;
+};
+
+// 派生类2：大明国家属性
+class CountryAttr : public Attribute
+{
+public:
+    CountryAttr(double initValue = 500, double min = 0, double max = 1000);
+    bool checkLimit() override;
+};
+
+// 派生类3：敌对/可控势力属性
+class ForceAttr : public Attribute
+{
+public:
+    ForceAttr(double initValue = 1000, double min = 0, double max = 2000);
+    bool checkLimit() override;
+};
+
+#endif // ATTRIBUTE_H
